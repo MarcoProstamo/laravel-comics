cff598a3ab6ee44161492cf1038181b1a46fd309
MarcoProstamo <Marcopros1201@gmail.com>
2025-03-03 15:26:38 +0100

    Add → Styled Navbar + Functional Navigation

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,23 +1,25 @@
 <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href={{ route("comics") }}>Navbar</a>
+        <div class="container">
+            <a class="navbar-brand" href={{ route("comics") }}>
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Logo" width="30" height="30">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href={{ route("comics") }}>Comics</a>
+                        <a class="nav-link {{ Route::is('comics') ? 'active' : '' }}" href={{ route("comics") }}>Comics</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href={{ route("news") }}>News</a>
+                        <a class="nav-link {{ Route::is('news') ? 'active' : '' }}" href={{ route("news") }}>News</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href={{ route("shop") }}>Shop</a>
+                        <a class="nav-link {{ Route::is('shop') ? 'active' : '' }}" href={{ route("shop") }}>Shop</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href={{ route("clown") }}>Shop</a>
+                        <a class="nav-link {{ Route::is('clown') ? 'active' : '' }}" href={{ route("clown") }}>Clown</a>
                     </li>
                 </ul>
             </div>
