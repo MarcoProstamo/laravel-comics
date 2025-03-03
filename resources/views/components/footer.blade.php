c01d7a8aebe79e4ba6153bfdd5e4450010c0702d
MarcoProstamo <Marcopros1201@gmail.com>
2025-03-03 15:41:59 +0100

    Add → Lower Footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,18 @@
-<div>
-    Ciao sono un Footer!
-</div>
+<footer>
+    <div class="upper-footer"></div>
+    <div class="lower-footer">
+        <div class="container py-3">
+            <div class="d-flex justify-content-between align-items-center">
+                <button class="btn btn-sign-up">SIGN-UP NOW!</button>
+                <div class="d-flex align-items-center">
+                    <span class="text-primary fw-semibold fs-4 me-4">FOLLOW US</span>
+                    <img class="me-3 cursor-pointer social-link" src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="Facebook" width="30" height="30">
+                    <img class="me-3 cursor-pointer social-link" src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="Twitter" width="30" height="30">
+                    <img class="me-3 cursor-pointer social-link" src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="YouTube" width="30" height="30">
+                    <img class="me-3 cursor-pointer social-link" src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="Pinterest" width="30" height="30">
+                    <img class="me-3 cursor-pointer social-link" src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="Periscope" width="30" height="30">
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
